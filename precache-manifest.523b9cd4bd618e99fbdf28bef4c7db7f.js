8325ff524b044830d1e3d3dbb21e4923acd216fe
akrock0512 <akrock0512000@gmail.com>
2020-08-09 05:30:47 +0530

    Updates

---
+++ precache-manifest.523b9cd4bd618e99fbdf28bef4c7db7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155258e1a0eb3ace7755d32d5824775d",
+    "url": "/worldcovid19data/index.html"
+  },
+  {
+    "revision": "65a4fb7848711ca99038",
+    "url": "/worldcovid19data/static/css/main.617cc047.chunk.css"
+  },
+  {
+    "revision": "53a71f7ef359747589b5",
+    "url": "/worldcovid19data/static/js/2.c51ded04.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/worldcovid19data/static/js/2.c51ded04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65a4fb7848711ca99038",
+    "url": "/worldcovid19data/static/js/main.a6a8b870.chunk.js"
+  },
+  {
+    "revision": "a6d6530543358c479f0a",
+    "url": "/worldcovid19data/static/js/runtime-main.04c34787.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/worldcovid19data/static/media/image.d7265326.png"
+  }
+]);
